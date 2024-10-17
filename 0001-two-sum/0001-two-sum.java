c7ed8f9577e6a82777dcb029e8d4e2138387392f
fahmykamal <103646953+fahmykamal@users.noreply.github.com>
2024-10-17 18:17:59 +0800

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -14,4 +14,26 @@ class Solution {
 
         return new int[] {};
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Example 1
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        System.out.println("Output: [" + result1[0] + ", " + result1[1] + "]"); // Output: [0, 1]
+
+        // Example 2
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        System.out.println("Output: [" + result2[0] + ", " + result2[1] + "]"); // Output: [1, 2]
+
+        // Example 3
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        System.out.println("Output: [" + result3[0] + ", " + result3[1] + "]"); // Output: [0, 1]
+    }
 }
